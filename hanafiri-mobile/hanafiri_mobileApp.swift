69a64170b887f6092ac0f7e900c0f781fc875279
shiorina <kuroiwa.1014.shiori@gmail.com>
2023-03-25 23:57:32 +0900

    Initial Commit

---
+++ hanafiri-mobile/hanafiri_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  hanafiri_mobileApp.swift
+//  hanafiri-mobile
+//
+//  Created by 黒岩詩織 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct hanafiri_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
